7aff37429c3c625cd773dc995b97fb3daad2123c
vitordelfrari <del23009@byui.edu>
2023-10-07 19:32:15 -0300

    Finished

--- scripts/w04-task.js
+++ scripts/w04-task.js
@@ -1,67 +1,66 @@
 /* LESSON 3 - Programming Tasks */
 document.addEventListener("DOMContentLoaded", function () {
 
-    /* Profile Object  */
-    let myProfile = {
-        name: "Vitor Del Frari",
-        photo: "images/placeholder.png",
-        favoriteFoods: [
-            "Barbecue", 
-            "Burguer", 
-            "Pizza"
-            ],
-        hobbies: [
-            "Basketball", 
-            "Swimming"
+/* Profile Object  */
+let myProfile = {
+    name: "Vitor Del Frari",
+    photo: "images/placeholder.png",
+    favoriteFoods: [
+        "Barbecue", 
+        "Burguer", 
+        "Pizza"
         ],
-        placesLived: [
-            
-        ]
-    };
+    hobbies: [
+        "Basketball", 
+        "Swimming"
+    ],
+    placesLived: [
+        
+    ]
+  };
 
-    /* Populate Profile Object with placesLive objects */
-    myProfile.placesLived.push(
-        { place: "Franca, Brazil", length: "16 year" },
-        { place: "Chicago, Illinois", length: "2 years" },
-        { place: "Natal, Brazil", length: "2 years" });
-
-
-    /* DOM Manipulation - Output */
-
-    /* Name */
-    document.querySelector('#name').textContent = myProfile.name;
-
-    /* Photo with attributes */
-    document.querySelector('#photo').src = myProfile.photo;
-    document.querySelector('#photo').alt = myProfile.name;
+/* Populate Profile Object with placesLive objects */
+myProfile.placesLived.push(
+    { place: "Franca, Brazil", length: "16 year" },
+    { place: "Chicago, Illinois", length: "2 years" },
+    { place: "Natal, Brazil", length: "2 years" });
 
 
-    /* Favorite Foods List*/
-    myProfile.favoriteFoods.forEach(food => {
-        let li = document.createElement('li');
-        li.textContent = food;
-        document.querySelector('#favorite-foods').appendChild(li);
-    });
+/* DOM Manipulation - Output */
 
-    /* Hobbies List */
-    myProfile.hobbies.forEach(hobby => {
-        let li = document.createElement('li');
-        li.textContent = hobby;
-        document.querySelector('#hobbies').appendChild(li);
-    });
-    
+/* Name */
+document.querySelector('#name').textContent = myProfile.name;
 
-    /* Places Lived DataList */
-    myProfile.placesLived.forEach(place => {
-        let dt = document.createElement('dt');
-        dt.textContent = place.place;
-    
-        let dd = document.createElement('dd');
-        dd.textContent = place.length;
-    
-        let dl = document.querySelector('#places-lived');
-        dl.appendChild(dt);
-        dl.appendChild(dd);
-    });
- });
-      
+/* Photo with attributes */
+document.querySelector('#photo').src = myProfile.photo;
+document.querySelector('#photo').alt = myProfile.name;
+
+
+/* Favorite Foods List*/
+myProfile.favoriteFoods.forEach(food => {
+    let li = document.createElement('li');
+    li.textContent = food;
+    document.querySelector('#favorite-foods').appendChild(li);
+  });
+
+/* Hobbies List */
+myProfile.hobbies.forEach(hobby => {
+    let li = document.createElement('li');
+    li.textContent = hobby;
+    document.querySelector('#hobbies').appendChild(li);
+  });
+  
+
+/* Places Lived DataList */
+myProfile.placesLived.forEach(place => {
+    let dt = document.createElement('dt');
+    dt.textContent = place.place;
+  
+    let dd = document.createElement('dd');
+    dd.textContent = place.length;
+  
+    let dl = document.querySelector('#places-lived');
+    dl.appendChild(dt);
+    dl.appendChild(dd);
+  });
+});
